fd35a517d9af087845e08bdc65c65299cdb50490
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2024-05-31 11:26:47 +0530

    Update Cors Configuration

--- server/app.js
+++ server/app.js
@@ -39,12 +39,7 @@ mongoose
 // Middleware
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
